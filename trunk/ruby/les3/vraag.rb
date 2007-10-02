84953db5f937886fde2320ff85f7661711867b56
wimpunk <wimpunk@4413d68c-223b-0410-a1fb-3389c1723150>
2007-10-02 17:36:00 +0000

    na correctie in de les

--- trunk/ruby/les3/vraag.rb
+++ trunk/ruby/les3/vraag.rb
@@ -1,21 +1,24 @@
  # vraag de naam van de gebruiker en beeld een welkomst bericht af
-  print "Graag jouw naam: "
-  naam = gets.chomp
-  printf("Welkom %s\n", naam)
-  
-  # vragen naar een geheel getal < 100
-  begin
-  print "Geef een geheel < 100:"
-  geheel = gets.to_i
-  end until ( geheel < 100 )
-  
-  # vragen naar een gebroken getal
-  print 'geef een gebroken getal van maximum 3 decimalen: '
-  fl = gets.to_f
-  #fl = Float(fl)
-  
-  # afbeelden
-  printf("\tgeheel: %1$5i\n\tgeheel: %1$05i\n\t float: %2$0.2f\n", geheel, fl);
-  
-
-# gets  
+ puts "Caramba, dit zie je in eclipse"
+ print "Graag jouw naam: "
+ naam = gets.chomp
+ printf("Welkom %s\n", naam)
+ # eigenlijk was ik naar deze op zoek
+ printf("Welkom #{naam}\n")
+ 
+ # vragen naar een geheel getal < 100
+ begin
+   print 'Geef een geheel < 100:'
+   geheel = gets.to_i
+ end until ( geheel < 100 )
+ 
+ # vragen naar een gebroken getal
+ print 'geef een gebroken getal van maximum 3 decimalen: '
+ fl = gets.to_f
+ #fl = Float(fl)
+ 
+ # afbeelden
+ printf("\tgeheel: %1$5i\n\tgeheel: %1$05i\n\t float: %2$0.2f\n", geheel, fl);
+ 
+ 
+ # gets  
