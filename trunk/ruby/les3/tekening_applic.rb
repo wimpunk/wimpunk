47520ea799fb5231a53ffd6d548c1226a5b10081
wimpunk <wimpunk@4413d68c-223b-0410-a1fb-3389c1723150>
2007-10-07 20:15:43 +0000

    eigen aanpassingen driehoek

--- trunk/ruby/les3/tekening_applic.rb
+++ trunk/ruby/les3/tekening_applic.rb
@@ -11,10 +11,7 @@ class Tekening_applic
   
   driehoek = Tekening.new(5,'x')
   # ik moet nog kijken naar het voorbeeld van de les
-  # maar dit zou ook moeten lukkne
-  mine.driehoek(5)
-  
-  s = String.new("Wim")
-  print s[1].to_s
+  # maar dit zou ook moeten lukken
+  1.upto(5) { |i| mine.driehoek(i) }
   
 end
