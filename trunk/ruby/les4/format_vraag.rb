896fa1d6ed357c966f2c7718796c6642b80a0e38
wimpunk <wimpunk@4413d68c-223b-0410-a1fb-3389c1723150>
2007-10-09 17:52:48 +0000

    mijn versie

---
+++ trunk/ruby/les4/format_vraag.rb
@@ -0,0 +1,25 @@
+class FormatVraag
+  # uitbreiding
+  
+  def initialize(vraag="een getal:")
+    print "#{vraag} "
+    @vraag = vraag
+    @input = gets.chomp
+  end
+  
+  def lijn
+    1.upto(@vraag.length+1+@input.length) do  
+      printf '-'
+    end  
+    printf "\n"
+  end
+  
+  def to_s
+    @input.to_s
+  end
+  
+  def to_i
+    @input.to_i
+  end
+  
+end
