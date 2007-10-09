4d5064ce62391a5afd9c333f9b42e3ed4835ff52
wimpunk <wimpunk@4413d68c-223b-0410-a1fb-3389c1723150>
2007-10-09 18:54:40 +0000

    while dag bijgevoegd

---
+++ ruby/intro/__while_dag.rb
@@ -0,0 +1,19 @@
+class WhileDag
+for dag in (1...8)
+    case (dag)
+    when 1 : puts('maandag')
+    when 2 : puts('dinsdag')
+    when 3 : puts('woensdag' )
+    when 4 : puts('donderdag' )
+    when 5 then puts('vrijdag')
+      puts("...bijna weekend!")
+    when 6, 7 
+      print('weekend ') if dag == 6
+      puts('- zaterdag' ) if dag == 7
+      puts('- zondag')
+    when 5 : puts( 'nogmaals vrijdag' )
+    else puts( 'ongeldige dag...')     
+    end
+  end
+
+end
