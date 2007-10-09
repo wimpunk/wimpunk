5701485345beeb53bfb8f4e49978cda0be0cfeb9
wimpunk <wimpunk@4413d68c-223b-0410-a1fb-3389c1723150>
2007-10-09 18:14:02 +0000

    een beetje bijgewerkt zoals in de les

--- trunk/ruby/les4/Getal.rb
+++ trunk/ruby/les4/Getal.rb
@@ -2,24 +2,29 @@
 # opgave pagina 22
 require "les4/format_vraag"
 
-print 'graag een getal '
+print '1. Graag een getal '
+msg = " => het getal %d is "
 input = gets.to_i
 if (input % 2 == 0)
   result = input / 2
   printf "Het getal #{input} is even, gedeeld door 2 = #{result}\n"
+  printf msg + "even, delen door 2 = %d\n", input, input/=2
 else 
   result = input+1
   printf "Het getal #{input} is oneven, eentje erbij = #{result}\n"
+  printf msg + "oneven, verhoogd met 1 = %d\n", input, input+=1
 end
 
-puts  '------------------------------'
-print 'graag een tweede getal (pos/neg) '
-input = gets.to_i
-printf "Het getal #{input} is %s dan 0\n#{input>0?groter:kleiner}\n", input>0?"groter":"kleiner"
+# een lijntje
+1.upto(25) do print '-' end
 
-str = FormatVraag.new("geef iets")
+print "\n"+'2. Graag een tweede getal (pos/neg) '
+input = gets.to_i
+printf "Het getal #{input} is %s dan 0\n", input>0?"groter":"kleiner"
+
+str = FormatVraag.new("3. geef iets")
 str.lijn()
-puts "#{str}"
+puts "Je gaf me <#{str}> als antwoord"
 
 str = FormatVraag.new()
 str.lijn()
@@ -27,6 +32,6 @@ puts str
 
 getal = FormatVraag.new('graag een tweede getal (pos/neg) ').to_i
 printf "getal  #{getal} is %s 0\n", 
-  getal>0?"groter dan":"kleiner dan of gelijk aan"
+getal>0?"groter dan":"kleiner dan of gelijk aan"
 
 
