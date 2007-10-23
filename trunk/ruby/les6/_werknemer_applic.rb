a1ac03ba5bc8b140fff443192aebf64cf588044f
wimpunk <wimpunk@4413d68c-223b-0410-a1fb-3389c1723150>
2007-10-23 17:57:43 +0000

    Met werkende teamleider (rofl)

--- trunk/ruby/les6/_werknemer_applic.rb
+++ trunk/ruby/les6/_werknemer_applic.rb
@@ -1,21 +1,32 @@
 #...
+require "les4/format_vraag"
+require "les6/werknemer"
 
 class Werknemer_applic
   def maak_lijst
     leider = true
     @lijst = Werknemer.enkel_naam('Jan'), Werknemer.new('Els', leider) \
-      , Werknemer.enkel_naam('Leen'), Werknemer.new('Piet', leider) 
+    , Werknemer.enkel_naam('Leen'), Werknemer.new('Piet', leider) 
   end
   
   def team_leiders
     lijst = []
-#. . .
+    cnt=0
+    
+    @lijst.each do
+      |member|
+      if member.leider
+        lijst[cnt] = member
+        cnt+=1
+      end
+    end
+    # lijst[] = @lijst
     return lijst
   end
   
   def geen_team_leiders
     lijst = []
-#. . .
+    #. . .
     return lijst
   end
   
@@ -25,4 +36,6 @@ applic = Werknemer_applic.new
 applic.maak_lijst
 puts '...: 1 :...'
 puts "de team leiders zijn: " 
+puts applic.team_leiders
 puts "volgende werknemers leiden geen team: "
+puts applic.geen_team_leiders
---
+++ trunk/ruby/les6/werknemer.rb
@@ -0,0 +1,19 @@
+class Werknemer
+  
+  def initialize naam='onbekend',functie=false 
+    @naam = naam
+    @functie = functie
+  end
+  
+  def self.enkel_naam naam='onbekend'
+    Werknemer.new(naam,false)
+  end
+  
+  def leider
+    @functie
+  end
+  
+  def to_s
+    @naam.to_s
+  end
+end
