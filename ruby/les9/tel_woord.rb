08972bcf6379512e7bfd4b13a1dcb574c312f1a5
wimpunk <wimpunk@4413d68c-223b-0410-a1fb-3389c1723150>
2007-11-20 17:58:15 +0000

    oplossing opgave 47.1

---
+++ ruby/les9/tel_woord.rb
@@ -0,0 +1,27 @@
+#class TelWoord
+#  
+#  def initialize
+#    super
+#  end
+#end
+
+zin = "wij willen Willem weg wil Willem wijzer worden wij willen Willem weer"
+
+puts zin
+hash = Hash.new()
+
+zin.each(' ') {|woord| 
+  woord.strip!
+  if hash[woord]
+  puts "#{woord} gevonden\n"
+    hash[woord]+=1
+  else
+  puts "#{woord} niet gevonden\n"
+    hash[woord]=1
+  end
+
+}
+
+
+puts "hash toestand"
+puts hash.inspect
