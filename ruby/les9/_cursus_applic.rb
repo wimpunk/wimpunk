9b841cfcc6cec9e498772b2d7cd8f9edaaefa46b
wimpunk <wimpunk@4413d68c-223b-0410-a1fb-3389c1723150>
2007-11-20 19:42:36 +0000

    toestand voor cursusopgave

---
+++ ruby/les9/_cursus_applic.rb
@@ -0,0 +1,36 @@
+require "collecties/cursus"
+class Cursus_applic
+  cursussen = [Cursus.new('Ruby - 1','Jan', 18.15, 10),
+  Cursus.new('Ruby - 2','Piet', 18.15, 8),
+  Cursus.new('Java - 1','Els', 14, 15),
+  Cursus.new('Java - 2','Jan', 14, 10),
+  Cursus.new('Java - 3','Piet', 18.15, 8)
+  ]
+  
+  puts '5. Alle Java-cursussen, daarna alle andere cursussen: '
+  puts '6. Alle cursussen gesorteerd op cursus naam: '
+  puts '7. Alle cursussen voorafgegaan met de index: '
+  
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
