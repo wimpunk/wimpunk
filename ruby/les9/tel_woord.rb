6ade4775399af24abc426472d50c004c9d61123e
wimpunk <wimpunk@4413d68c-223b-0410-a1fb-3389c1723150>
2007-11-20 18:20:51 +0000

    een aantal mogelijkheden bijgevoegd om af te drukken

--- ruby/les9/tel_woord.rb
+++ ruby/les9/tel_woord.rb
@@ -1,27 +1,37 @@
-#class TelWoord
-#  
-#  def initialize
-#    super
-#  end
-#end
+class Woord_Teller
+  
+  def initialize zin
+    @zin = zin
+  end
+  
+  def split
+    # aanmaken van de lege hash
+    hash = Hash.new()
+    
+    # splitten op de spatie
+    @zin.each(' ') {|woord| 
+      # verwijderen van de eventuele laatste blanco
+      woord.strip!
+      # als het woord niet gevonden is: initieren op 1
+      hash[woord]=0 if ! hash[woord]
+      hash[woord]+=1
+    }
+    
+    return hash
+  end
+  
+  def to_s
+    self.split.inspect
+  end
+  
+  def inspect
+    myhash = self.split
+    myhash.each {|a| printf "woord %s komt %i keer voor\n", a[0], a[1]}
+  end
+end
 
 # zin die we gebruiken
-zin = "wij willen Willem weg wil Willem wijzer worden wij willen Willem weer"
-
-# aanmaken van de lege hash
-hash = Hash.new()
-
-# splitten op de spatie
-zin.each(' ') {|woord| 
-  # verwijderen van de eventuele laatste blanco
-  woord.strip!
-  # als het woord niet gevonden is: initieren op 1
-  if ! hash[woord]
-    hash[woord]=1
-  else # in het andere geval tellen we er eentje bij
-    hash[woord]+=1
-  end
-}
-
-puts "hash toestand"
-puts hash.inspect
+telwoord = Woord_Teller.new "wij willen Willem weg wil Willem wijzer worden wij willen Willem weer"
+puts telwoord.split.inspect
+puts telwoord
+telwoord.inspect
