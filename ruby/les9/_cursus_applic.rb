f05d77521079b054d9d58e3d1edb6783ae8da443
wimpunk <wimpunk@4413d68c-223b-0410-a1fb-3389c1723150>
2007-11-26 21:19:16 +0000

    print_lijst bijgevoegd

--- ruby/les9/_cursus_applic.rb
+++ ruby/les9/_cursus_applic.rb
@@ -1,5 +1,10 @@
 require "les9/_cursus"
 
+
+def print_lijst lijst
+  lijst.each {|curs| puts curs}
+end
+
 class Cursus_applic
   cursussen = [
   Cursus.new('Ruby - 1','Jan', 18.15, 10),
@@ -10,15 +15,16 @@ class Cursus_applic
   ]
   
   puts '5. Alle Java-cursussen, daarna alle andere cursussen: '
-  ruby_cursussen,andere_cursussen = cursussen.partition{|cursus|
+  java_cursussen,andere_cursussen = cursussen.partition{|cursus|
     cursus.naam?('Java')
   }
-  ruby_cursussen.each {|curs| puts curs}
-  andere_cursussen.each {|curs| puts curs}
+  print_lijst java_cursussen
+  print_lijst andere_cursussen
   
   puts '6. Alle cursussen gesorteerd op cursus naam: '
-  cursussen.sort.each { |curs| puts curs }
+  print_lijst cursussen.sort
   
+  # TODO: hier ook print_lijst voor gebruiken.
   puts '7. Alle cursussen voorafgegaan met de index: '
   cursussen.each_index { |key| puts "#{key} : #{cursussen[key]}"}
   
